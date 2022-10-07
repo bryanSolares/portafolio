a7c84c262d9ab5e873f53fb1f2e827ac50f292ee
Bryan Josué Solares Pérez <bryan.solares@claro.com.gt>
2022-10-06 21:38:23 -0600

    Primer Despliegue

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Bryan Solares</h1>
+      <h3 className="home__subtitle">Backend Programmer</h3>
+      <p className="home__description">
+        Soy un apasionado a la técnología, me encanta crear soluciones que puedan ser utilizadas por las personas. Soy
+        apasionado en mi trabajo y mi mayor objetivo siempre es estar en constante aprendizaje.
+      </p>
+
+      <a href="#contact" className="button button--flex">
+        Saludame 📫
+      </a>
+    </div>
+  );
+};
+
+export default Data;

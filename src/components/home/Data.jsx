864e1f24b822e3fbdf075fcba801972f9ad1617e
BRYAN JOSUE SOLARES PEREZ <bryan.solares@claro.com.gt>
2024-05-17 10:30:02 -0600

    style: :art: Adding icon for main page
    Adding icon for main page

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Bryan Solares</h1>
+      <h5 className="home__subtitle">
+        AWS Solutions Architect <br /> Backend developer
+      </h5>
+      <p className="home__description">
+        Me apasiona la ingeniería del caos, los temas relacionados a seguridad en las aplicaciones e implementación sin
+        negociación de los requerimientos no funcionales relacionados a un arquitecto de soluciones. Durante mis más de
+        5 años de experiencia, he priorizado la excelencia en cada proyecto, buscando destacar en lo ordinario y no
+        conformarme con lo convencional. Mi objetivo es innovar, superar expectativas y dejar una huella positiva en
+        cada proyecto en el que participo.
+      </p>
+
+      <a href="mailto:solares.josue@outlook.com" className="button button--flex">
+        Saludame si lo deseas 📫
+      </a>
+    </div>
+  );
+};
+
+export default Data;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,15 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.linkedin.com/in/bryan-solares/" className="home__social-icon" rel="noopener">
+        <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/bryanSolares" className="home__social-icon" rel="noopener">
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;

3f83c9154981d4feff14d95705c0e81ded4e25fd
BRYAN JOSUE SOLARES PEREZ <bryan.solares@claro.com.gt>
2024-05-31 11:23:03 -0600

    style: :art: Adding Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Projects from "./components/projects/Projects";
+import Experience from "./components/experience/Experience";
+import Skills from "./components/skills/Skills";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        {/* <Projects /> */}
+        <Experience />
+        {/* <Skills /> */}
+        <Contact />
       </main>
     </>
   );
